68fd3121f53d347dc4fdd1f0e9ce4eb251daea36
Ayoub Nejjari <raysoflee07@gmail.com>
2017-06-05 22:47:12 +0000

    main component and testing. Things are working!

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+
+
+class App extends Component {
+  render() {
+    return (
+      <h1>Test</h1>
+    );
+  }
+}
+
+export default App
