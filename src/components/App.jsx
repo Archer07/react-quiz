51d997bedfa1722a20993b910e1c41a47852aa6b
Ayoub Nejjari <raysoflee07@gmail.com>
2017-06-05 23:57:57 +0000

    adding quiz questions

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,17 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 
+import Questions from './quiz/questions.js'
+
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    // hardcoded data
+    this.state = {
+      questions: Questions
+    }
+  }
   render() {
     return (
       <h1>Test</h1>
---
+++ src/components/quiz/questions.js
@@ -0,0 +1,78 @@
+let Questions = [
+    {
+      "id": 1,
+      "question": "What's the capital of France?",
+      "choices": [
+      {
+        "id":"a",
+        "text": "London"
+      },
+      {
+        "id":"b",
+        "text": "Vienna"
+      },
+      {
+        "id":"c",
+        "text": "Paris"
+      }
+    ],
+    "correct":"a"
+    },
+    {
+      "id": 2,
+      "question": "How many states are in the USA?",
+      "choices": [
+      {
+        "id":"a",
+        "text": "47"
+      },
+      {
+        "id":"b",
+        "text": "50"
+      },
+      {
+        "id":"c",
+        "text": "53"
+      }
+    ],
+    "correct":"b"
+  },
+    {
+      "id": 3,
+      "question": "Who invented the World Wide Web?",
+      "choices": [
+      {
+        "id":"a",
+        "text": "Brendan Eich"
+      },
+      {
+        "id":"b",
+        "text": "Tim Berners-Lee"
+      },
+      {
+        "id":"c",
+        "text": "Bill Gates"
+      }],
+      "correct":"b"
+    },
+    {
+      "id": 4,
+      "question": "How many countries are there in our World?",
+      "choices": [{
+        "id":"a",
+        "text": "197"
+      },
+      {
+        "id":"b",
+        "text": "193"
+      },
+      {
+        "id":"c",
+        "text": "195"
+      }
+    ],
+    "correct":"c"
+  }
+];
+
+export defalt Questions
